25acf1c61021c65f1ed666645f6c79aabf2cf7bc
benceHornyak <sir.bence15@gmail.com>
2018-01-21 16:32:42 +0100

    Added test for empty scopes

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ test('should decorate request instance with jwtAuthz method', function (t) {
   fastify.register(jwtAuthz)
 
   fastify.get('/test', function (request, reply) {
-    t.ok(request.jwtAuthz)
+    t.ok(request.jwtAuthz)    
     reply.send({ foo: 'bar' })
   })
   fastify.listen(0, function (err) {
@@ -20,6 +20,30 @@ test('should decorate request instance with jwtAuthz method', function (t) {
     request({
       method: 'GET',
       uri: 'http://localhost:' + fastify.server.address().port + '/test',
+      json: true
+    }, function (err, response) {
+      t.error(err)
+      t.ok(response)
+    })
+  })
+})
+
+test('should throw an error \"Scopes cannot be empty\" with an empty scopes parameter', function (t) {
+  t.plan(4)
+  var fastify = Fastify()
+  fastify.register(jwtAuthz)
+
+  fastify.get('/test2', function (request, reply) {
+    request.jwtAuthz([])
+      .catch(err => t.match(err.message, 'Scopes cannot be empty') )
+    reply.send({ foo: 'bar' })
+  })
+  fastify.listen(0, function (err) {
+    fastify.server.unref()
+    t.error(err)
+    request({
+      method: 'GET',
+      uri: 'http://localhost:' + fastify.server.address().port + '/test2',
       json: true
     }, function (err, response) {
       t.error(err)
